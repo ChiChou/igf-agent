3793bcf7e6056fc38a9faab22af9762834ebcb9b
cc <codecolorist@gmail.com>
2025-06-30 01:09:58 +0200

    use a seperated repo for agent

---
+++ README.md
@@ -0,0 +1,15 @@
+# igf-agent
+
+Frida agent script for [igf](https://npmjs.org/igf).
+
+Main repository: [grapefruit](https://github.com/chichou/grapefruit)
+
+## Build
+
+```npm run build```
+
+## Run test
+
+Require a jailbroken iOS device with frida installed.
+
+```npm test```
---
+++ fruity/modules/checksec.ts
@@ -0,0 +1,21 @@
+import { fromBytes } from "../bridge/dictionary.js";
+import { encryptionInfo, pie } from "../lib/macho.js";
+
+export interface CheckSecFlags {
+  pie: boolean;
+  arc: boolean;
+  canary: boolean;
+  encrypted: boolean;
+}
+
+export function flags(): CheckSecFlags {
+  const main = Process.mainModule;
+  const uniqueNames = new Set(main.enumerateImports().map(({ name }) => name));
+
+  return {
+    pie: pie(main),
+    arc: uniqueNames.has("objc_release"),
+    canary: uniqueNames.has("__stack_chk_guard"),
+    encrypted: encryptionInfo(main)?.cryptid === 1,
+  };
+}
