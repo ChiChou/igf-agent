b553a413a9bec614c8c95b1cd7e4c2c28e29d92c
cc <codecolorist@gmail.com>
2025-11-03 22:20:34 +0100

    fix prepare error upgrade dependencies

--- scripts/prepare.ts
+++ scripts/prepare.ts
@@ -1,8 +1,13 @@
 import cp from "node:child_process";
+import fs from "node:fs/promises";
+import path from "node:path";
 
 import { allBuildScripts, root } from "./utils.ts";
 
 async function run() {
+  const dist = path.join(root, "dist");
+  await fs.access(dist).catch(() => fs.mkdir(dist));
+
   for await (const [name, cmd] of allBuildScripts()) {
     console.log("run", name);
     cp.spawn(`npx ${cmd} -c`, {
